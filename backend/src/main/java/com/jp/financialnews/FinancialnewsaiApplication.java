911efdcade99fdccc067d6ee4cbf32ea9731a52e
jngs-park <dear.jayp@gmail.com>
2026-03-14 15:21:55 +0900

    add fastapi ai service & gitignore add

--- backend/src/main/java/com/jp/financialnews/FinancialnewsaiApplication.java
+++ backend/src/main/java/com/jp/financialnews/FinancialnewsaiApplication.java
@@ -3,8 +3,6 @@ package com.jp.financialnews;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class FinancialnewsaiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinancialnewsaiApplication.class, args);
